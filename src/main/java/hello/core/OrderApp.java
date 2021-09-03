a87e2044ccb32f0e012074fe5d0347a69752abfc
dongkyu <kimdongkyu5902@hanmail.net>
2021-09-03 09:30:24 +0900

    스프링 컨텍스트에 빈을 등록하여, 구현체를 꺼내어쓰기

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,14 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = configApplicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService =  configApplicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -19,5 +20,4 @@ public class OrderApp {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("order = " + order);
     }
-
 }
